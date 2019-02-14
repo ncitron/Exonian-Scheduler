f91cd4c6ef212b4a2ea61895d024a1304426085c
PeterTuchler <47619470+PeterTuchler@users.noreply.github.com>
2019-02-13 22:47:59 -0500

    Added Appointment Class

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,6 +1,41 @@
+import java.util.Date;
 
 public class Appointment {
-	Appointment() {
-		System.out.println("This is some test code");
+	
+	private Date startTime;
+	private Date endTime;
+	private String appName;
+	private int appType;
+	
+	public Appointment() {
+		Date now = new Date();
+		startTime = now; //Current time and date --> probably should be changed
+		endTime = now;	// ^^^
+		appName = "";
+		appType = 0;
 	}
+	public Appointment(Date theStart, Date theEnd, String theName, int theType) {
+		startTime = theStart;
+		endTime = theEnd;
+		appName = theName;
+		appType = theType;
+	}
+	
+	
+	public String toString() {
+		String strType = "" + appType;
+		return startTime.toString() + " " + endTime.toString() + " " + appName + " " + strType;
+	}
+	
+	public String toDisplayString() {
+		return "";
+	}
+
+	public static void main(String[] args) {
+		Appointment app = new Appointment();
+		
+		System.out.print(app.toString());
+		
+	}
+
 }
